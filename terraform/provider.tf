6722b4e1aeceef6c28c7199be85a05ab85009c9e
Robbie Douglas <rld244@cornell.edu>
2022-03-16 16:39:21 -0400

    Update provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      # version = "~> 3.70"
+      source  = "hashicorp/aws"
+      version = "~> 3.70"
     }
   }
 }
@@ -11,7 +11,7 @@ provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
   region                      = "us-east-1"
-  s3_force_path_style         = false
+  s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
