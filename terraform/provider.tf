14367f6675147f6da79e2ab8eea26d6d0fcf1f1c
Robbie Douglas <rld244@cornell.edu>
2022-03-16 16:35:44 -0400

    Add required provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # version = "~> 3.70"
+    }
+  }
+}
+
 provider "aws" {
   access_key                  = "test"
   secret_key                  = "test"
